3d9752b87b50222d11217df9b2d977e2ce6d5f31
Daniel A. Wozniak <dwozniak@vmware.com>
2022-09-12 21:29:22 -0700

    Fix fetch

--- mayflower/fetch.py
+++ mayflower/fetch.py
@@ -11,5 +11,6 @@ def main(argparser):
     )
     url = "https://woz.io/mayflower/build/x86_64-linux-gnu.tar.xz"
     builddir = pathlib.Path("build").resolve()
+    os.makedirs(builddir, exist_ok=True)
     archive = download_url(url, builddir)
     extract_archive(builddir, archive)
