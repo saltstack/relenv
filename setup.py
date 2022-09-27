adfc5aff776df24e19ac8c7e745503641dc29afd
Daniel A. Wozniak <dwozniak@vmware.com>
2022-09-27 16:44:31 -0700

    not ready for this

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(package_data={'mayflower': ['_build/*.tar.xz', '_toolchain/*tar.xz']}, include_package_data=True)
+    setup(package_data={
+       # XXX make this a separate package
+       #'mayflower': ['_build/*.tar.xz', '_toolchain/*tar.xz']},
+    include_package_data=True)
