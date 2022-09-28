f52ae194800e429e3fc8a1366715bea2a26dfc5b
Daniel A. Wozniak <dwozniak@vmware.com>
2022-09-27 17:10:16 -0700

    Fix syntax wart

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(package_data={
        # XXX make this a separate package
-       #'mayflower': ['_build/*.tar.xz', '_toolchain/*tar.xz']},
+       #'mayflower': ['_build/*.tar.xz', '_toolchain/*tar.xz']
+    },
     include_package_data=True)
