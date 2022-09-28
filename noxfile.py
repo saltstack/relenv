62219ef0d3968be116e252d1939cdf6cb2db76f8
MKLeb <calebb@vmware.com>
2022-09-28 16:38:10 -0400

    add toolchain and build sessions and use them in CICD workflows

--- noxfile.py
+++ noxfile.py
@@ -8,20 +8,8 @@ import os
 import pathlib
 import sys
 
-# fmt: off
-if __name__ == "__main__":
-    sys.stderr.write(
-        "Do not execute this file directly. Use nox instead, it will know how to handle this file\n"
-    )
-    sys.stderr.flush()
-    exit(1)
-# fmt: on
-
 import nox  # isort:skip
 
-# Be verbose when runing under a CI context
-CI_RUN = os.environ.get("CI") is None
-PIP_INSTALL_SILENT = CI_RUN is False
 SKIP_REQUIREMENTS_INSTALL = os.environ.get("SKIP_REQUIREMENTS_INSTALL", "0") == "1"
 
 # Global Path Definitions
@@ -29,6 +17,10 @@ REPO_ROOT = pathlib.Path(os.path.dirname(__file__)).resolve()
 os.chdir(str(REPO_ROOT))
 
 ARTIFACTS_DIR = REPO_ROOT / "artifacts"
+PYTEST_LOGFILE = ARTIFACTS_DIR.joinpath(
+    "logs",
+    "pytest-{}.log".format(datetime.datetime.now().strftime("%Y%m%d%H%M%S.%f")),
+)
 
 # Nox options
 #  Reuse existing virtualenvs
@@ -36,27 +28,58 @@ nox.options.reuse_existing_virtualenvs = True
 #  Don't fail on missing interpreters
 nox.options.error_on_missing_interpreters = False
 
-PYTEST_LOGFILE = ARTIFACTS_DIR.joinpath(
-    "logs",
-    "pytest-{}.log".format(datetime.datetime.now().strftime("%Y%m%d%H%M%S.%f")),
-)
 
 # Prevent Python from writing bytecode
 os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
 
 
 # <---------------------- SESSIONS ---------------------->
+@nox.session
+def tests(session):
+    run_pytest_session(session)
 
 
 @nox.session
-def tests(session):
-    session.install("pytest")
-    run_pytest_session(session)
+@nox.parametrize("arch", ("x86_64", "aarch64"))
+def build(session, arch):
+    invoke_mayflower(session, "toolchain", "download", f"--arch={arch}")
+    invoke_mayflower(session, "build", f"--arch={arch}")
+
+
+@nox.session
+@nox.parametrize("arch", ("x86_64", "aarch64"))
+def toolchain(session, arch):
+    invoke_mayflower(session, "toolchain", "build", f"--arch={arch}")
+
+
+# Convenience sessions
+@nox.session
+def build_x86_64(session):
+    session.notify("build(arch='x86_64')")
+
+
+@nox.session
+def build_aarch64(session):
+    session.notify("build(arch='aarch64')")
+
+
+@nox.session
+def toolchain_x86_64(session):
+    session.notify("toolchain(arch='x86_64')")
+
+
+@nox.session
+def toolchain_aarch64(session):
+    session.notify("toolchain(arch='aarch64')")
 
 
 # <---------------------- HELPERS ---------------------->
 def run_pytest_session(session, *cmd_args):
     make_artifacts_directory()
+
+    if not SKIP_REQUIREMENTS_INSTALL:
+        session.install("pytest")
+
     default_args = [
         "-vv",
         "--showlocals",
@@ -75,6 +98,10 @@ def run_pytest_session(session, *cmd_args):
 
     pytest_args = default_args + list(cmd_args)
     session.run("python", "-m", "pytest", *pytest_args)
+
+
+def invoke_mayflower(session, *cmd_args):
+    session.run("python", "-m", "mayflower", *cmd_args)
 
 
 def make_artifacts_directory():
