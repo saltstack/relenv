2af593c6e80521beaea9a1caff8425e04bd360e9
Daniel A. Wozniak <dwozniak@vmware.com>
2022-10-13 14:41:46 -0700

    Add build portion to toolchain docs

--- docs/source/toolchain.rst
+++ docs/source/toolchain.rst
@@ -1,6 +1,6 @@
 
 Toolchains
-==========
+##########
 
 Mayflower uses toolchains to compile Python (and it's dependencies) on Linux platforms. These toolchains consist of GCC, Binutils, and GLibc and are built using `crosstool-ng`_. Mayflower's toolchains are pre-built. Users of Mayflower will only need a toolchain when installing C extensions which need to re-main portable accross multiple Linux OSes. When working with pure python applications users should not need to concern themselves with toolchains.
 
@@ -16,6 +16,19 @@ Building toolchains is a farily expensive and lengthy process. It's recommended 
 
    sudo yum -y groupinstall "Development Tools"
    sudo yum -y --enablerepo=powertools install vim python3 texinfo help2man ncurses-devel
+
+
+Running Mayflower's toolchain build command will do the following
+
+* Download crosstool-ng
+* Conigure and compile crosstool-ng
+* Use Mayflower's crosstool config files to compile the requests architectures
+
+.. code-block:: bash
+
+   git clone git@github.com:saltstack/mayflower.git
+   cd mayflower
+   python3 -m mayflower toolchain build --arch=x86_64,aarch64
 
 
 .. _crosstool-ng: https://crosstool-ng.github.io/
--- mayflower/toolchain.py
+++ mayflower/toolchain.py
@@ -4,14 +4,12 @@ import platform
 import sys
 
 from .common import (
-    MODULE_DIR,
     download_url,
     extract_archive,
     get_toolchain,
     get_triplet,
     runcmd,
     work_dirs,
-    work_root,
 )
 
 WORK_IN_CWD = False
@@ -54,6 +52,24 @@ def setup_parser(subparsers):
         help="When building only build Crosstool NG. Do not build toolchains",
     )
 
+def download(arch, toolchain, clean=False):
+    """
+    Download a toolchain and extract it to the filesystem.
+
+    :param str arch: the architecture of the toolchain
+    :param str toolchain: where to extract the toolchain
+    """
+    triplet = get_triplet(arch)
+    archdir = get_toolchain(arch)
+    if clean:
+        shutil.rmtree(archdir)
+    if archdir.exists():
+        print("Toolchain directory exists, skipping {}".format(arch))
+    url = TC_URL.format(version="0.0.0", host=platform.machine(), triplet=triplet)
+    print("Downloading {}".format(url))
+    archive = download_url(url, toolchain)
+    extract_archive(toolchain, archive)
+
 
 def main(args):
     args.arches = {_.lower() for _ in args.arches}
@@ -65,26 +81,26 @@ def main(args):
         os.makedirs(toolchain)
     if args.command == "download":
         for arch in args.arches:
-            triplet = get_triplet(arch)
-            archdir = get_toolchain(arch)
-            if args.clean:
-                shutil.rmtree(archdir)
-            if archdir.exists():
-                print("Toolchain directory exists, skipping {}".format(arch))
-            url = TC_URL.format(
-                version="0.0.0", host=platform.machine(), triplet=triplet
-            )
-            print("Downloading {}".format(url))
-            archive = download_url(url, toolchain)
-            extract_archive(toolchain, archive)
+            download(arch, dirs.toolchain, ns.clean)
         sys.exit(0)
     elif args.command == "build":
         ctngdir = toolchain / "crosstool-ng-{}".format(CT_NG_VER)
+    dirs = work_dirs()
+    if not dirs.toolchain.exists():
+        os.makedirs(dirs.toolchain)
+    if ns.command == "download":
+        for arch in ns.arch:
+            download(arch, dirs.toolchain, ns.clean)
+        sys.exit(0)
+    elif ns.command == "build":
+        ctngdir = dirs.toolchain / "crosstool-ng-{}".format(CT_NG_VER)
         if not ctngdir.exists():
             url = CT_URL.format(version=CT_NG_VER)
-            archive = download_url(url, toolchain)
-            extract_archive(toolchain, archive)
-            os.chdir(ctngdir)
+            archive = download_url(url, dirs.toolchain)
+            extract_archive(dirs.toolchain, archive)
+        os.chdir(ctngdir)
+        ctng = ctngdir / "ct-ng"
+        if not ctng.exists():
             runcmd(["./configure", "--enable-local"])
             runcmd(["make"])
             os.chdir(toolchain)
@@ -93,11 +109,11 @@ def main(args):
         ctng = ctngdir / "ct-ng"
         for arch in args.arches:
             triplet = get_triplet(arch)
-            archdir = toolchain / triplet
+            archdir = dirs.toolchain / triplet
             if archdir.exists():
                 print("Toolchain directory exists: {}".format(arch))
                 continue
-            config = toolchain / machine / "{}-ct-ng.config".format(triplet)
+            config = dirs.toolchain / machine / "{}-ct-ng.config".format(triplet)
             if not config.exists():
                 print("Toolchain config missing: {}".format(config))
                 sys.exit(1)
@@ -105,10 +121,10 @@ def main(args):
                 with open(".config", "w") as wfp:
                     wfp.write(rfp.read())
             env = os.environ.copy()
-            env["CT_PREFIX"] = toolchain
+            env["CT_PREFIX"] = dirs.toolchain
             runcmd(
                 [
-                    ctng,
+                    str(ctng),
                     "build",
                 ],
                 env=env,
