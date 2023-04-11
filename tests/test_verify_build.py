cee27fad734a32519ffe8d8a7d23f742f3d3ce83
Gareth J. Greenaway <gareth@saltstack.com>
2023-04-11 10:20:05 -0700

    swap /bin/sh for /bin/bash in tests where source is used.

--- tests/test_verify_build.py
+++ tests/test_verify_build.py
@@ -540,7 +540,7 @@ def test_install_pycurl(pipexec, build, minor_version, tmpdir):
             )
         )
 
-    subprocess.run(["/bin/sh", "buildcurl.sh"], check=True)
+    subprocess.run(["/bin/bash", "buildcurl.sh"], check=True)
 
     # Make sure curl-config exists.
     assert (build / "bin" / "curl-config").exists()
@@ -654,7 +654,7 @@ def test_install_libgit2(pipexec, build, minor_version, tmpdir, versions):
             )
         )
 
-    subprocess.run(["/bin/sh", "buildscript.sh"], check=True)
+    subprocess.run(["/bin/bash", "buildscript.sh"], check=True)
 
     env = os.environ.copy()
     env["RELENV_DEBUG"] = "yes"
@@ -716,7 +716,7 @@ def test_install_python_ldap(pipexec, build, minor_version, tmpdir):
             )
         )
 
-    subprocess.run(["/bin/sh", "buildscript.sh"], check=True)
+    subprocess.run(["/bin/bash", "buildscript.sh"], check=True)
     env = os.environ.copy()
     env["RELENV_DEBUG"] = "yes"
     env["RELENV_BUILDENV"] = "yes"
