739b0b22dd575f9c522118701adc813578d52dc0
Pedro Algarvio <palgarvio@vmware.com>
2023-05-04 18:04:21 +0100

    Include `PYTHONPATH` entries in the mix
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/test_runtime.py
+++ tests/test_runtime.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 import logging
+import os
 import sys
 from unittest.mock import patch
 
@@ -11,12 +12,15 @@ log = logging.getLogger(__name__)
 
 
 def test_wrap_sitecustomize():
-    expected = sorted(["/foo/1", "/bar/2"])
+    python_path_entries = ["/blah/blah", "/yada/yada"]
+    expected = ["/foo/1", "/bar/2"] + python_path_entries
     assert sorted(sys.path) != expected
     with patch.object(sys, "prefix", "/foo"), patch.object(
         sys, "base_prefix", "/bar"
-    ), patch.object(sys, "path", ["/foo/1", "/bar/2", "/lib/3"]):
-        assert sorted(sys.path) != expected
+    ), patch.object(sys, "path", ["/foo/1", "/bar/2", "/lib/3"]), patch.dict(
+        os.environ, PYTHONPATH=os.pathsep.join(python_path_entries)
+    ):
+        assert sys.path != expected
         relenv.runtime.wrapsitecustomize(lambda: True)()
-        assert sorted(sys.path) == expected
-    assert sorted(sys.path) != expected
+        assert sys.path == expected
+    assert sys.path != expected
