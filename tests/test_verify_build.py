2944dab48ae7ca17dd4c53a6a5408e4a2f84b416
Daniel A. Wozniak <dwozniak@vmware.com>
2023-08-26 16:07:24 -0700

    Only parse version if defined

--- tests/test_verify_build.py
+++ tests/test_verify_build.py
@@ -4,7 +4,6 @@
 Verify relenv builds.
 """
 import os
-import packaging
 import pathlib
 import platform
 import shutil
@@ -12,6 +11,7 @@ import subprocess
 import sys
 import textwrap
 
+import packaging
 import pytest
 
 from relenv.common import DATA_DIR, get_triplet, list_archived_builds, plat_from_triplet
@@ -174,7 +174,10 @@ def test_pip_install_salt_git(pipexec, build, tmp_path, pyexec):
 @pytest.mark.skip_on_darwin
 @pytest.mark.skip_on_windows
 @pytest.mark.skipif(
-    packaging.version.parse(get_build_version()) >= packaging.version.parse("3.11.4"), reason="3.11.4 will not work with 3005.x"
+    get_build_version()
+    and packaging.version.parse(get_build_version())
+    >= packaging.version.parse("3.11.4"),
+    reason="3.11.4 will not work with 3005.x",
 )
 def test_pip_install_salt(pipexec, build, tmp_path, pyexec):
     packages = [
