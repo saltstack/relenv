87f58ddfb90352203cfc586b08e070d95f8dce30
Daniel A. Wozniak <dwozniak@vmware.com>
2023-09-08 18:23:47 -0700

    Add fips mode test

---
+++ tests/conftest.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import platform
+
+import pytest
+
+from relenv.common import list_archived_builds, plat_from_triplet
+from relenv.create import create
+
+
+def get_build_version():
+    if "RELENV_PY_VERSION" in os.environ:
+        return os.environ["RELENV_PY_VERSION"]
+    builds = list(list_archived_builds())
+    versions = []
+    for version, arch, plat in builds:
+        sysplat = plat_from_triplet(plat)
+        if sysplat == sys.platform and arch == platform.machine().lower():
+            versions.append(version)
+    if versions:
+        return versions[0]
+
+
+@pytest.fixture(scope="module")
+def build_version():
+    return get_build_version()
+
+
+@pytest.fixture(scope="module")
+def minor_version():
+    yield get_build_version().rsplit(".", 1)[0]
+
+
+@pytest.fixture
+def build(tmp_path, build_version):
+    create("test", tmp_path, version=build_version)
+    yield tmp_path / "test"
+
+
+@pytest.fixture
+def pipexec(build):
+    if sys.platform == "win32":
+        exc = build / "Scripts" / "pip3.exe"
+    else:
+        exc = build / "bin" / "pip3"
+    yield exc
+
+
+@pytest.fixture
+def pyexec(build):
+    if sys.platform == "win32":
+        exc = build / "Scripts" / "python.exe"
+    else:
+        exc = build / "bin" / "python3"
+    yield exc
---
+++ tests/test_fips_photon.py
@@ -0,0 +1,50 @@
+import os
+import pathlib
+import subprocess
+
+import pytest
+
+from .conftest import get_build_version
+
+
+def check_test_environment():
+    path = pathlib.Path("/etc/os-release")
+    if path.exists():
+        release = path.read_text()
+        return "Photon" in release and "4.0" in release
+    return False
+
+
+pytestmark = [
+    pytest.mark.skipif(not get_build_version(), reason="Build archive does not exist"),
+    pytest.mark.skipif(
+        not check_test_environment(), reason="Not running on photon 4 with fips enabled"
+    ),
+]
+
+
+def test_fips_mode(pyexec, build):
+    env = os.environ.copy()
+    proc = subprocess.run(
+        [
+            pyexec,
+            "-c",
+            "import hashlib; hashlib.sha256(b'')",
+        ],
+        check=False,
+        env=env,
+        capture_output=True,
+    )
+    assert proc.returncode == 0
+    assert b"ValueError" not in proc.stderr
+    proc = subprocess.run(
+        [
+            pyexec,
+            "-c",
+            "import hashlib; hashlib.md5(b'')",
+        ],
+        check=False,
+        env=env,
+        capture_output=True,
+    )
+    assert b"ValueError" in proc.stderr or b"UnsupportedDigestmodError" in proc.stderr
