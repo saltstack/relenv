163c9cabfb484f23e1ba8580b06a5b7c6a5dd7e2
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2025-03-01 15:09:20 -0700

    Fix build unit test

--- tests/test_build.py
+++ tests/test_build.py
@@ -29,8 +29,6 @@ def test_builder_defaults_linux():
     assert builder.prefix == DATA_DIR / "build" / "3.10.10-x86_64-linux-gnu"
     assert builder.sources == DATA_DIR / "src"
     assert builder.downloads == DATA_DIR / "download"
-    assert "ppbt" in str(builder.toolchains)
-    assert "_toolchain" in str(builder.toolchains)
     assert "ppbt" in str(builder.toolchain)
     assert "_toolchain" in str(builder.toolchain)
     assert callable(builder.build_default)
