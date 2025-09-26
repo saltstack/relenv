5a77a963ffeb589a3ff9a3af011e5d71abf9cb7b
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2025-09-26 00:28:44 -0700

    Fix compiling with newest ncurses

--- relenv/pyversions.py
+++ relenv/pyversions.py
@@ -21,7 +21,7 @@ import subprocess
 import sys
 import time
 
-from relenv.common import check_url, download_url, fetch_url_content
+from relenv.common import Version, check_url, download_url, fetch_url_content
 
 log = logging.getLogger(__name__)
 
@@ -41,102 +41,6 @@ def _ref_version(x):
 
 def _ref_path(x):
     return x.split('href="')[1].split('"')[0]
-
-
-class Version:
-    """
-    Version comparrisons.
-    """
-
-    def __init__(self, data):
-        self.major, self.minor, self.micro = self.parse_string(data)
-        self._data = data
-
-    def __str__(self):
-        """
-        Version as string.
-        """
-        _ = f"{self.major}"
-        if self.minor is not None:
-            _ += f".{self.minor}"
-            if self.micro is not None:
-                _ += f".{self.micro}"
-        # XXX What if minor was None but micro was an int.
-        return _
-
-    @staticmethod
-    def parse_string(data):
-        """
-        Parse a version string into major, minor, and micro integers.
-        """
-        parts = data.split(".")
-        if len(parts) == 1:
-            return int(parts[0]), None, None
-        elif len(parts) == 2:
-            return int(parts[0]), int(parts[1]), None
-        elif len(parts) == 3:
-            return int(parts[0]), int(parts[1]), int(parts[2])
-        else:
-            raise RuntimeError("Too many parts to  parse")
-
-    def __eq__(self, other):
-        """
-        Equality comparrison.
-        """
-        mymajor = 0 if self.major is None else self.major
-        myminor = 0 if self.minor is None else self.minor
-        mymicro = 0 if self.micro is None else self.micro
-        major = 0 if other.major is None else other.major
-        minor = 0 if other.minor is None else other.minor
-        micro = 0 if other.micro is None else other.micro
-        return mymajor == major and myminor == minor and mymicro == micro
-
-    def __lt__(self, other):
-        """
-        Less than comparrison.
-        """
-        mymajor = 0 if self.major is None else self.major
-        myminor = 0 if self.minor is None else self.minor
-        mymicro = 0 if self.micro is None else self.micro
-        major = 0 if other.major is None else other.major
-        minor = 0 if other.minor is None else other.minor
-        micro = 0 if other.micro is None else other.micro
-        if mymajor < major:
-            return True
-        elif mymajor == major:
-            if myminor < minor:
-                return True
-            if myminor == minor and mymicro < micro:
-                return True
-        return False
-
-    def __le__(self, other):
-        """
-        Less than or equal to comparrison.
-        """
-        mymajor = 0 if self.major is None else self.major
-        myminor = 0 if self.minor is None else self.minor
-        mymicro = 0 if self.micro is None else self.micro
-        major = 0 if other.major is None else other.major
-        minor = 0 if other.minor is None else other.minor
-        micro = 0 if other.micro is None else other.micro
-        if mymajor <= major:
-            if myminor <= minor:
-                if mymicro <= micro:
-                    return True
-        return False
-
-    def __gt__(self, other):
-        """
-        Greater than comparrison.
-        """
-        return not self.__le__(other)
-
-    def __ge__(self, other):
-        """
-        Greater than or equal to comparrison.
-        """
-        return not self.__lt__(other)
 
 
 def _release_urls(version, gzip=False):
